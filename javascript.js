415c6797a422e2dc0a2923fad0ca9734ff3b964a
AtikahRohim <74558644+AtikahRohim@users.noreply.github.com>
2021-01-10 02:31:15 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Javascript</title>
+</head>
+<body>
+<script type="text/javascript">
+alert("Welcome to Cuisine Culture! This is the place that you can order your food.");
+</script>
+
+</body>
+</html>
